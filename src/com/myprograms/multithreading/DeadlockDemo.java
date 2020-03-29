d1127b552a61f24442d14cc5402937894f40a3f5
mitaly <senmitaly13@gmail.com>
2020-03-29 09:08:25 +0530

    deadlock and detection

--- src/com/myprograms/multithreading/DeadlockDemo.java
+++ src/com/myprograms/multithreading/DeadlockDemo.java
@@ -1,9 +1,36 @@
 package com.myprograms.multithreading;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 public class DeadlockDemo {
+	private static ScheduledFuture scheduledFuture;
+	
 	public static void main(String[] args) {
 		final String resource1 = "resource 1";
 		final String resource2 = "resource 2";
+
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+		
+		scheduledFuture = executor.scheduleAtFixedRate(() -> {
+			ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+			long[] deadlockedThreadIds = threadMXBean.findDeadlockedThreads();
+			if (deadlockedThreadIds != null && deadlockedThreadIds.length > 0) {
+				ThreadInfo[] threadInfoArr = threadMXBean.getThreadInfo(deadlockedThreadIds);
+				
+				for (ThreadInfo info : threadInfoArr) {
+					System.out.println(info.getThreadName());
+				}
+				
+				scheduledFuture.cancel(true);
+			}
+
+		}, 0, 1000, TimeUnit.MILLISECONDS);
 
 		Thread thread1 = new Thread() {
 			@Override
